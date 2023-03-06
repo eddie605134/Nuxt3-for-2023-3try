fab45ad8a5f57bc11cdfdc62788fc574a42b8a18
eddie.chang <eddie605134@gmail.com>
2023-03-06 14:37:32 +0800

    to middleware

--- app.vue
+++ app.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <ClientOnly>
-      <IronManWelcome />
-      <template #fallback>
-        <p class="my-6 flex justify-center">[IronManWelcome] 載入中...</p>
-      </template>
-    </ClientOnly>
-  </div>
+  <!-- <div class="py-24 bg-white">
+    <div class="flex justify-center">
+      <Icon name="logos:nuxt" size="360" />
+    </div>
+    <div class="flex flex-col items-center">
+      <h1 class="my-2 text-6xl font-semibold text-sky-400">{{ title }}</h1>
+      <h1 class="my-2 text-6xl font-semibold text-emerald-400">{{ $hello('Jennifer') }}</h1>
+    </div>
+    <div class="flex justify-center">
+      <button
+        v-focus
+        class="w-[100px] border-t-neutral-50 bg-slate-600 text-neutral-700 focus:text-white"
+      >
+        測試
+      </button>
+    </div>
+  </div> -->
+  <NuxtPage />
 </template>
+
+<script lang="ts" setup>
+const { $hello } = useNuxtApp()
+const title = $hello('Ryan')
+</script>
 
 <!-- <template>
   <div class="flex flex-col items-center">
